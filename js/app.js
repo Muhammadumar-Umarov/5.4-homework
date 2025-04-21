8ea3cd2f4e4c0120aee6bb69464349aaa8f83782
Muhammadumar-Umarov <surnameen22@gmail.com>
2025-04-21 15:45:26 +0500

    updated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 const mainImage = document.querySelector(".main__img")
+const card = document.querySelector(".card")
+const cardInfo = document.querySelector(".card_info")
+const cardActions = document.querySelector(".card_actions")
 
 function changeImg(path) {
     mainImage.setAttribute("src", path)
+}
+
+
+function showFullCard(){
+    card.classList.toggle("big__card")
+    cardInfo.classList.toggle("card__info")
+    cardActions.classList.toggle("card__actions")
 }
